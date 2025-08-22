c1c713ff8b85b06afcd9606417de36b04a813b3a
Ben Gabay <ben.gabay38@gmail.com>
2025-08-23 00:55:31 +0300

    fix bug when settings initialized outside main (#3)
    * Fix bug when settings initialized outside main
    
    * Remove call from settings.py

--- src/main.py
+++ src/main.py
@@ -2,12 +2,13 @@ import asyncio
 import logging
 
 from src.logging_setup import LoggerHandlerType, SetupLoggerParams, setup_logger
-from src.settings import settings
+from src.settings import AppSettings
 
 logger = logging.getLogger(__name__)
 
 
 async def main() -> None:
+    settings = AppSettings()  # type: ignore[call-arg]
     setup_logger(
         SetupLoggerParams(
             level=settings.logging.min_log_level,
